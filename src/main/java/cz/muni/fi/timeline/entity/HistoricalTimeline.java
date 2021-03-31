3a5831e0fdd9846ed29da53b419bfc4955468f1b
Ondřej Kuhejda <ondrejkuhejda@gmail.com>
2021-04-01 01:35:01 +0200

    Implement TimelineComment

--- src/main/java/cz/muni/fi/timeline/entity/HistoricalTimeline.java
+++ src/main/java/cz/muni/fi/timeline/entity/HistoricalTimeline.java
@@ -1,5 +1,8 @@
 package cz.muni.fi.timeline.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Collections;
 import java.util.HashSet;
@@ -10,6 +13,7 @@ import java.util.Set;
 /**
  * Entity class that represents historical timeline.
  * Historical events can be added and removed from this timeline.
+ * Also comments can be added and removed from this timeline.
  *
  * @author Ondřej Kuhejda
  */
@@ -18,30 +22,25 @@ public class HistoricalTimeline {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
     private Long id;
 
+    @Getter
+    @Setter
     private String name;
 
     @OneToMany
     @JoinColumn
     private final Set<HistoricalEvent> historicalEvents = new HashSet<>();
 
-    public Long getId() {
-        return id;
-    }
+    @OneToMany
+    @JoinColumn
+    private final Set<TimelineComment> timelineComments = new HashSet<>();
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
+    /**
+     * Returns all events in this timeline.
+     */
     public Set<HistoricalEvent> getHistoricalEvents() {
         return Collections.unmodifiableSet(historicalEvents);
     }
@@ -60,16 +59,37 @@ public class HistoricalTimeline {
         historicalEvents.remove(event);
     }
 
+    /**
+     * Returns all comments in this timeline.
+     */
+    public Set<TimelineComment> getTimelineComments() {
+        return Collections.unmodifiableSet(timelineComments);
+    }
+
+    /**
+     * Adds new comment to the timeline.
+     */
+    public void addTimelineComment(TimelineComment comment) {
+        timelineComments.add(comment);
+    }
+
+    /**
+     * Removes comment from the timeline.
+     */
+    public void removeTimelineComment(TimelineComment comment) {
+        timelineComments.remove(comment);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof HistoricalTimeline)) return false;
         HistoricalTimeline that = (HistoricalTimeline) o;
-        return Objects.equals(name, that.name);
+        return Objects.equals(getName(), that.getName());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return Objects.hash(getName());
     }
 }
