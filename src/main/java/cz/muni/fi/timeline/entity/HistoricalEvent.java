4867e1b6ff5572ca674aa65e26d7bb8f4bfde90f
Ondřej Kuhejda <ondrejkuhejda@gmail.com>
2021-03-31 23:54:46 +0200

    Annotate entities with Entity annotation

--- src/main/java/cz/muni/fi/timeline/entity/HistoricalEvent.java
+++ src/main/java/cz/muni/fi/timeline/entity/HistoricalEvent.java
@@ -1,8 +1,18 @@
 package cz.muni.fi.timeline.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Lob;
 
+
+@Entity
 public class HistoricalEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Lob
     private byte[] image;
--- src/main/java/cz/muni/fi/timeline/entity/Student.java
+++ src/main/java/cz/muni/fi/timeline/entity/Student.java
@@ -1,4 +1,15 @@
 package cz.muni.fi.timeline.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
