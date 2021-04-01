a057d36b05378f039172a5b067f38ca7a87ca562
Tri Le Mau <tlemau@bjsw.cz>
2021-04-01 20:32:31 +0200

    implemented Student & StudyGroup

--- src/main/java/cz/muni/fi/timeline/entity/Student.java
+++ src/main/java/cz/muni/fi/timeline/entity/Student.java
@@ -1,10 +1,19 @@
 package cz.muni.fi.timeline.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * This entity class represents a student
+ * Contains study groups where student is enrolled in
+ *
+ * @author Tri Le Mau
+ */
 
 @Entity
 public class Student {
@@ -12,4 +21,43 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Getter
+    @Setter
+    private String firstName;
+
+    @NotNull
+    @Getter
+    @Setter
+    private String lastName;
+
+    @NotNull
+    @Getter
+    @Setter
+    private String username;
+
+    @NotNull
+    @Getter
+    @Setter
+    private int hashedPassword;
+
+    @ManyToMany(mappedBy = "students")
+    private Set<StudyGroup> studyGroups;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student that = (Student) o;
+        return Objects.equals(getFirstName(), that.getFirstName())
+                && Objects.equals(getLastName(), that.getLastName())
+                && Objects.equals(getUsername(), that.getUsername())
+                && Objects.equals(getHashedPassword(), that.getHashedPassword());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFirstName(), getLastName(), getUsername(), getHashedPassword());
+    }
 }
--- src/main/java/cz/muni/fi/timeline/entity/StudyGroup.java
+++ src/main/java/cz/muni/fi/timeline/entity/StudyGroup.java
@@ -1,10 +1,19 @@
 package cz.muni.fi.timeline.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * This entity class represents a study group
+ * Contains students of the study group
+ *
+ * @author Tri Le Mau
+ */
 
 @Entity
 public class StudyGroup {
@@ -12,4 +21,25 @@ public class StudyGroup {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Getter
+    @Setter
+    private String name;
+
+    @ManyToMany
+    private Set<Student> students;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof StudyGroup)) return false;
+        StudyGroup that = (StudyGroup) o;
+        return Objects.equals(getName(), that.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
+    }
 }
