5d30ff915aba839383ffdb8ca546660e4872bc6e
Mato <469393@mail.muni.cz>
2021-04-01 13:50:00 +0200

    Add HistoricalEvent

--- src/main/java/cz/muni/fi/timeline/entity/HistoricalEvent.java
+++ src/main/java/cz/muni/fi/timeline/entity/HistoricalEvent.java
@@ -1,19 +1,66 @@
 package cz.muni.fi.timeline.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Objects;
 
-
+/**
+ * Entity class representing concrete historic event
+ * Contains name, brief description, data location and can contain image or placeholder
+ * @author Matej Macák
+ */
 @Entity
 public class HistoricalEvent {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
     private Long id;
 
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    private String description;
+
+    @Getter
+    @Setter
+    private LocalDate data;
+
+    @Getter
+    @Setter
+    private String location;
+
     @Lob
+    @Getter
+    @Setter
     private byte[] image;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof HistoricalEvent)) return false;
+        HistoricalEvent that = (HistoricalEvent) o;
+        return Objects.equals(getName(), that.getName())
+                && Objects.equals(getDescription(), that.getDescription())
+                && Objects.equals(getData(), that.getData())
+                && Objects.equals(getLocation(), that.getLocation())
+                && Arrays.equals(getImage(), that.getImage());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getDescription(), getData(), getLocation(), getImage());
+    }
 }
