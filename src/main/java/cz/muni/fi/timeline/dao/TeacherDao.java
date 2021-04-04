3c2e173d677e6ff85dbc721b388b924bd08beab1
Ondřej Kuhejda <ondrejkuhejda@gmail.com>
2021-04-04 23:13:31 +0200

    Improve javadoc for TeacherDao

--- src/main/java/cz/muni/fi/timeline/dao/TeacherDao.java
+++ src/main/java/cz/muni/fi/timeline/dao/TeacherDao.java
@@ -14,31 +14,45 @@ public interface TeacherDao {
 
     /**
      * Adds new teacher to the database.
+     *
+     * @param teacher Teacher that will be created.
      */
     void create(Teacher teacher);
 
     /**
      * Returns all teachers from the database.
+     *
+     * @return All teachers.
      */
     List<Teacher> findAll();
 
     /**
      * Finds teacher based on id.
+     *
+     * @param id Id that is used for finding teachers.
+     * @return Optional of teacher with given id.
      */
     Optional<Teacher> findById(Long id);
 
     /**
      * Finds teacher based on username.
+     *
+     * @param username Username that is used for finding teachers.
+     * @return Optional of teacher with given username.
      */
     Optional<Teacher> findByUsername(String username);
 
     /**
      * Updates values for some teacher in database.
+     *
+     * @param teacher Detached teacher that will be updated.
      */
     void update(Teacher teacher);
 
     /**
      * Removes teacher from the database.
+     *
+     * @param teacher Teacher that will be removed.
      */
     void remove(Teacher teacher);
 }
