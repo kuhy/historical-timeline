1284a50e910102c4561108ca9e12c90fd424cdcf
Karolina6 <60822073+Karolina42@users.noreply.github.com>
2021-05-16 23:24:22 +0200

    Rest comment, timeline, event

--- timeline-rest/src/main/java/cz/muni/fi/timeline/rest/controller/HistoricalEventController.java
+++ timeline-rest/src/main/java/cz/muni/fi/timeline/rest/controller/HistoricalEventController.java
@@ -1,7 +1,27 @@
 package cz.muni.fi.timeline.rest.controller;
 
+import cz.muni.fi.timeline.api.dto.HistoricalEventDTO;
+
+import cz.muni.fi.timeline.entity.HistoricalEvent;
+import cz.muni.fi.timeline.rest.assembler.HistoricalEventAssembler;
+
+import cz.muni.fi.timeline.rest.exceptions.ResourceNotFoundException;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import cz.muni.fi.timeline.api.HistoricalTimelineFacade;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Optional;
 
 
 /**
@@ -10,15 +30,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/events")
 public class HistoricalEventController {
-    // TODO getEvent
-    // GET /{id}
 
-    // TODO removeEvent
-    // DELETE /{id}
+    private final HistoricalTimelineFacade historicalTimelineFacade;
+    private final HistoricalEventAssembler historicalEventAssembler;
 
-    // TODO updateEvent
-    // PUT /{id}
+    @Inject
+    public HistoricalEventController(HistoricalTimelineFacade historicalTimelineFacade, HistoricalEventAssembler historicalEventAssembler) {
+        this.historicalTimelineFacade = historicalTimelineFacade;
+        this.historicalEventAssembler = historicalEventAssembler;
+    }
 
-    // TODO getAllEvents
-    // GET /
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final HttpEntity<EntityModel<HistoricalEventDTO> getEvent(@PathVariable("id") long id) {
+        Optional <HistoricalEventDTO> historicalEventDTO = historicalTimelineFacade.getHistoricalEventWithId(id);
+        if (!historicalEventDTO.isPresent()) {
+            throw new ResourceNotFoundException();
+        }
+        return new ResponseEntity<>(historicalEventAssembler.toModel(historicalEventDTO.get()), HttpStatus.OK);
+    }
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final ResponseEntity<Void> removeEvent(@PathVariable("id") long id) {
+        historicalTimelineFacade.removeHistoricalEvent(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    @RequestMapping(value = "/{id}",method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final ResponseEntity<Long> updateEvent(@PathVariable("id") long id) {
+        HistoricalEventDTO historicalEventDTO = historicalTimelineFacade.getHistoricalEventWithId(id).get();
+        return new ResponseEntity<>(historicalTimelineFacade.updateHistoricalEvent(historicalEventDTO), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final HttpEntity<CollectionModel<EntityModel<HistoricalEvent>>> getEvents() {
+        List<HistoricalEventDTO> eventDTOS = historicalTimelineFacade.getAllHistoricalEvents();
+
+        CollectionModel<EntityModel<HistoricalEventDTO>> eventCollectionModel = historicalEventAssembler.toCollectionModel(eventDTOS);
+        return new ResponseEntity<>(eventCollectionModel, HttpStatus.OK);
+    }
 }
--- timeline-rest/src/main/java/cz/muni/fi/timeline/rest/controller/TimelineCommentController.java
+++ timeline-rest/src/main/java/cz/muni/fi/timeline/rest/controller/TimelineCommentController.java
@@ -1,7 +1,23 @@
 package cz.muni.fi.timeline.rest.controller;
 
+import cz.muni.fi.timeline.api.HistoricalTimelineFacade;
+import cz.muni.fi.timeline.api.dto.TimelineCommentDTO;
+import cz.muni.fi.timeline.rest.exceptions.ResourceNotFoundException;
+import cz.muni.fi.timeline.rest.assembler.TimelineCommentAssembler;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Optional;
 
 
 /**
@@ -10,15 +26,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/comments")
 public class TimelineCommentController {
-    // TODO getComment
-    // GET /{id}
 
-    // TODO removeComment
-    // DELETE /{id}
+    private final HistoricalTimelineFacade historicalTimelineFacade;
+    private final TimelineCommentAssembler timelineCommentAssembler;
 
-    // TODO updateComment
-    // PUT /{id}
+    @Inject
+    public TimelineCommentController(HistoricalTimelineFacade historicalTimelineFacade, TimelineCommentAssembler timelineCommentAssembler) {
+        this.historicalTimelineFacade = historicalTimelineFacade;
+        this.timelineCommentAssembler = timelineCommentAssembler;
+    }
 
-    // TODO getAllComments
-    // GET /
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final HttpEntity<EntityModel<TimelineCommentDTO>> getComment(@PathVariable("id") long id) {
+        Optional<TimelineCommentDTO> timelineCommentDTO =  historicalTimelineFacade.getTimelineCommentWithId(id);
+        if (!timelineCommentDTO.isPresent()){
+            throw new ResourceNotFoundException();
+        }
+        return new ResponseEntity<>(timelineCommentAssembler.toModel(timelineCommentDTO.get()), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final ResponseEntity<Void> removeComment(@PathVariable("id") long id) {
+        historicalTimelineFacade.removeHistoricalTimeline(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final ResponseEntity<Long> updateComment(@PathVariable("id") long id) {
+        TimelineCommentDTO timelineCommentDTO = historicalTimelineFacade.getTimelineCommentWithId(id).get();
+        return new ResponseEntity<>(historicalTimelineFacade.updateTimelineComment(timelineCommentDTO),HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final HttpEntity<CollectionModel<TimelineCommentDTO>> getComments() {
+        List<TimelineCommentDTO> timelineCommentDTOS =  historicalTimelineFacade.getAllTimelineComments();
+        CollectionModel<EntityModel<TimelineCommentDTO>> commentCollectionModel = timelineCommentAssembler.toCollectionModel(timelineCommentDTOS);
+        return new ResponseEntity<>(commentCollectionModel,HttpStatus.OK);
+    }
 }
