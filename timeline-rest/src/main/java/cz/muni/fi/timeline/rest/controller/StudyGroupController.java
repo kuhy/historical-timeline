a7719859f5bcc4e5dc0c2185c9830effea6b54b0
Mato <469393@mail.muni.cz>
2021-05-16 20:36:58 +0200

    Work on REST

--- timeline-rest/src/main/java/cz/muni/fi/timeline/rest/controller/StudyGroupController.java
+++ timeline-rest/src/main/java/cz/muni/fi/timeline/rest/controller/StudyGroupController.java
@@ -1,7 +1,24 @@
 package cz.muni.fi.timeline.rest.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import cz.muni.fi.timeline.api.StudyGroupFacade;
+import cz.muni.fi.timeline.api.dto.HistoricalTimelineCreateDTO;
+import cz.muni.fi.timeline.api.dto.StudyGroupCreateDTO;
+import cz.muni.fi.timeline.api.dto.StudyGroupDTO;
+import cz.muni.fi.timeline.api.exception.UserAlreadyInStudyGroupException;
+import cz.muni.fi.timeline.api.exception.UserNotInStudyGroupException;
+import cz.muni.fi.timeline.entity.HistoricalTimeline;
+import cz.muni.fi.timeline.rest.assembler.StudyGroupAssembler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.InvalidParameterException;
+import java.util.List;
+import java.util.Optional;
 
 
 /**
@@ -10,27 +27,105 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/groups")
 public class StudyGroupController {
-    // TODO getStudyGroup
+
+    final static Logger logger = LoggerFactory.getLogger(StudyGroupController.class);
+
+    private StudyGroupFacade studyGroupFacade;
+    private StudyGroupAssembler studyGroupAssembler;
+
+    @Autowired
+    public StudyGroupController(StudyGroupFacade groupFacade, StudyGroupAssembler studyGroupAssembler){
+        this.studyGroupFacade = groupFacade;
+        this.studyGroupAssembler = studyGroupAssembler;
+    }
+
+    // getStudyGroup
     // GET /{id}
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final Optional<StudyGroupDTO> getStudyGroup(@PathVariable("id") long id){
+        logger.debug("rest getStudyGroup({})", id);
+        Optional<StudyGroupDTO> studyGroupDTO = studyGroupFacade.getStudyGroupWithId(id);
+        return studyGroupDTO;
+    }
 
-    // TODO removeStudyGroup
+    // removeStudyGroup
     // DELETE /{id}
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final void deleteStudyGroup(@PathVariable("id") long id) throws Exception {
+        logger.debug("rest deleteStudyGroup({})", id);
+        try {
+            studyGroupFacade.removeStudyGroup(id);
+        } catch (Exception ex) {
+            throw new Exception("resource not found");
+        }
+    }
 
-    // TODO updateStudyGroup
+    // updateStudyGroup
     // PUT /{id}
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public final Optional<StudyGroupDTO> changePrice(@PathVariable("id") long id, @RequestBody StudyGroupDTO updatedGroup) throws Exception {
 
-    // TODO getAllStudyGroups
+        logger.debug("rest updateStudyGroup({})", id);
+
+        try {
+            studyGroupFacade.updateStudyGroup(updatedGroup);
+            return studyGroupFacade.getStudyGroupWithId(id);
+        } catch (Exception ex) {
+            throw new InvalidParameterException();
+        }
+
+    }
+
+    // getAllStudyGroups
     // GET /
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final List<StudyGroupDTO> getAllStudyGroups() {
+        logger.debug("rest getAllStudyGroups()");
+        return studyGroupFacade.getAllStudyGroups();
+    }
 
-    // TODO createStudyGroup
+    //  createStudyGroup
     // POST /create
+    @RequestMapping(value = "/create", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public final Optional<StudyGroupDTO> createStudyGroup(@RequestBody StudyGroupCreateDTO studyGroupCreateDTO) throws Exception {
+
+        logger.debug("rest createStudyGroup()");
+
+        try {
+            Long id = studyGroupFacade.createStudyGroup(studyGroupCreateDTO);
+            return studyGroupFacade.getStudyGroupWithId(id);
+        } catch (Exception ex) {
+            throw new Exception("Resource already exists");
+        }
+    }
 
     // TODO createTimelineInStudyGroup
     // POST /{id}/timelines/create
 
-    // TODO removeUserFromStudyGroup
+    @RequestMapping(value = "/{id}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final Optional<StudyGroupDTO> createTimelineInStudyGroup(HistoricalTimelineCreateDTO historicalTimelineCreateDTO, long groupId){
+        Long id = studyGroupFacade.createTimelineInStudyGroup(historicalTimelineCreateDTO, groupId);
+        return studyGroupFacade.getStudyGroupWithId(id);
+    }
+    
+    // removeUserFromStudyGroup
     // DELETE /users/{id}
+    //TODO not so sure about this
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final void removeUserFromStudyGroup(@PathVariable("id") long studyGroupId, @PathVariable("id") long userId) throws UserNotInStudyGroupException {
+        logger.debug("rest removeUserFromStudyGroup");
+
+        studyGroupFacade.removeUserFromStudyGroup(studyGroupId, userId);
+    }
 
     // TODO addUserToStudyGroup
     // PUT /users/{id}
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final void addUserToStudyGroup(@PathVariable("id") long studyGroupId, @PathVariable("id") long userId) throws UserNotInStudyGroupException, UserAlreadyInStudyGroupException {
+        logger.debug("rest addUserFromStudyGroup");
+
+        studyGroupFacade.addUserToStudyGroup(studyGroupId, userId);
+    }
 }
