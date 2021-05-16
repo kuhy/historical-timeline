5633f20cc97b0ab530aeff3dea00dbde119bd1c7
Mato <469393@mail.muni.cz>
2021-05-16 20:36:58 +0200

    Work on REST

--- timeline-rest/src/main/java/cz/muni/fi/timeline/rest/assembler/StudyGroupAssembler.java
+++ timeline-rest/src/main/java/cz/muni/fi/timeline/rest/assembler/StudyGroupAssembler.java
@@ -2,8 +2,15 @@ package cz.muni.fi.timeline.rest.assembler;
 
 
 import cz.muni.fi.timeline.api.dto.StudyGroupDTO;
+import cz.muni.fi.timeline.rest.controller.StudyGroupController;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 /**
  * @author Matej Macák
@@ -11,6 +18,14 @@ import org.springframework.hateoas.server.RepresentationModelAssembler;
 public class StudyGroupAssembler implements RepresentationModelAssembler<StudyGroupDTO, EntityModel<StudyGroupDTO>> {
     @Override
     public EntityModel<StudyGroupDTO> toModel(StudyGroupDTO entity) {
-        return EntityModel.of(entity);
+        EntityModel<StudyGroupDTO> groupModel = new EntityModel<>(entity);
+        try{
+            groupModel.add(linkTo(StudyGroupController.class).slash(entity.getId()).withSelfRel());
+            groupModel.add(linkTo(StudyGroupController.class).slash(entity.getId()).withRel("DELETE"));
+        } catch (Exception ex){
+            Logger.getLogger(StudyGroupAssembler.class.getName()).log(Level.SEVERE, "could not link resource from StudyGroupController", ex);
+        }
+        return groupModel;
     }
+
 }
